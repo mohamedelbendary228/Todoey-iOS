b0c58b653f9fa01b73363a704ccdc0b0b041884d
mohamedelbendary <mohamedelbendary228@gmail.com>
2026-06-23 14:58:35 +0300

    Add CategoryViewController and create a relationship with items

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,91 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Mohamed Elbendary on 22/06/2026.
+//  Copyright © 2026 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class CategoryViewController: UITableViewController {
+    
+    var categoriesArray :[Category] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if #available(iOS 26.0, *) {
+            navigationItem.rightBarButtonItem?.hidesSharedBackground = true
+        }
+        
+        loadCategories()
+    }
+    
+    
+    // MARK: - Save Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Category", style: .default) { action in
+            let newCategory = Category(context: self.context)
+
+            newCategory.name = textField.text
+            
+            self.categoriesArray.append(newCategory)
+            
+            self.saveCategories()
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create New Category"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true)
+        
+    }
+        
+        // MARK: - TableView DataSource Methods
+        
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return categoriesArray.count
+        }
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+            
+            cell.textLabel?.text = categoriesArray[indexPath.row].name
+            
+            return cell
+        }
+        
+        
+        // MARK: - Data Manipulation Methods
+        
+        func saveCategories() {
+            do {
+                try context.save()
+            } catch {
+                print("Error saving context: \(error)")
+            }
+            self.tableView.reloadData()
+        }
+        
+        func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+            do {
+                categoriesArray = try context.fetch(request)
+            } catch {
+                print("Error fetching data from context: \(error)")
+            }
+            tableView.reloadData()
+        }
+        
+        
+        // MARK: - TableView Delegate Methods
+    }
